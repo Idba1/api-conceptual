85f63a4c073b1ae93aca9cf50a90540c412bf8a9
Idba1 <moniraislamself@gmail.com>
2024-03-03 05:35:43 +0600

    load indevidual data from api link

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 // console.log('hello');
+
+ const loadCategory = async() =>{
+    const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await response.json();
+    // console.log(data.data.news_category);
+    data.data.news_category.forEach((item) => {
+        console.log(item)
+    })
+ }
+ loadCategory();
