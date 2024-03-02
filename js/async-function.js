a0d7c0d58c1a0319cbebd8e21110711173b7cc4f
Idba1 <moniraislamself@gmail.com>
2024-03-03 04:46:44 +0600

    design from github

--- js/async-function.js
+++ js/async-function.js
@@ -5,9 +5,9 @@ const sum = a+b;
 console.log("hello from console");
 
 const test = async () => {
-    const responsse = await fetch ('https://openapi.programming-hero.com/api/news/categories');
-    // console.log(responsse);
-    const data = await responsse.json();
+    const response = await fetch ('https://openapi.programming-hero.com/api/news/categories');
+    // console.log(response);
+    const data = await response.json();
     console.log(data);
 }
 test();
