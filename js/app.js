e893266625fca1f4efd7a99d2580aa41eb0d1cc5
Idba1 <moniraislamself@gmail.com>
2024-03-03 07:19:50 +0600

    show dynamic data

--- js/app.js
+++ js/app.js
@@ -20,9 +20,62 @@ const loadNews = async() =>{
     const response = await fetch('https://openapi.programming-hero.com/api/news/category/01');
     const data = await response.json();
     // console.log(data.data);
-    data.data.forEach((item) => {
-        console.log(item);
-    })
+    const newsContainer = document.getElementById("news-container");
+    data.data.forEach((singleNews) => {
+        // console.log(item);
+        const div = document.createElement("div");
+        div.classList.add("singleNews");
+        div.innerHTML = `
+    
+            <div class="news-photo">
+              <img
+                src=${singleNews.image_url}
+                alt=""
+              />
+            </div>
+            <div class="news-info">
+              <div class="news-header">
+                <h4>${singleNews.title.slice(0, 20)}</h4>
+                <p class="news-badge">
+                ${singleNews.rating.badge} <sup> <h6 class="news-rating"> ${
+          singleNews.rating.number
+        }</h6></sup>
+                </p>
+              </div>
+              <p>
+              ${singleNews.details.slice(0, 200)}
+              </p>
+    
+              <div class="news-footer">
+                <div class="author">
+                  <div class="">
+                    <img
+                      class="author-img"
+                      src=${singleNews.author.img}
+                      alt=""
+                    />
+                  </div>
+                  <div class="author-info">
+                    <h6> ${singleNews.author.name}</h6>
+                    <p>Date: ${singleNews.author.published_date}</p>
+                  </div>
+                </div>
+                <div class="Views author">
+                  <img
+                    class="view-img"
+                    src="https://uxwing.com/wp-content/themes/uxwing/download/health-sickness-organs/view-icon.png"
+                    alt=""
+                  />
+                  <p>${singleNews.total_view}</p>
+                </div>
+                <div class="details-btn-container">
+                  <button onclick="handleDetails(${JSON.stringify({name:"gias"})})" class="details-btn">Details</button>
+                </div>
+            </div>
+          </div>
+          `;
+        newsContainer.appendChild(div);
+      });
 }
 loadNews();
 loadCategory();
