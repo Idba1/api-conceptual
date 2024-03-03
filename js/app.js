4155eb6bbe6caf076aa91bff61338e987e0c26cb
Idba1 <moniraislamself@gmail.com>
2024-03-03 06:05:17 +0600

    show all category in btn

--- js/app.js
+++ js/app.js
@@ -4,8 +4,15 @@
     const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
     const data = await response.json();
     // console.log(data.data.news_category);
+
+    // get container where you append child
+    const categoryContainer = document.getElementById('category-bar-container');
+
     data.data.news_category.forEach((item) => {
-        console.log(item)
+        // console.log(item);
+        const div = document.createElement('div');
+        div.innerHTML = `<button>${item.category_name}</button>`;
+        categoryContainer.appendChild(div);
     })
  }
  loadCategory();
